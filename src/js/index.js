7786907c3b8a973628c8fcd5e2617cd899eb0795
Aliaksei Siankevich <andfly@tut.by>
2021-08-11 17:20:08 +0300

    add: add burger

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 import '../sass/style.scss';
+
+const burger = document.querySelector('.burger');
+const burgerList = document.querySelector('.burger-list');
+
+burger.addEventListener('click', () => {
+  
+  burger.classList.toggle('burger-active');
+  burgerList.classList.toggle('burger-list-active');
+
+})
+
+
+
+
