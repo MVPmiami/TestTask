f171b16c6404962ee707b813ca75b80a7babfb58
Aliaksei Siankevich <andfly@tut.by>
2021-08-11 19:04:53 +0300

    add: added adaptive for mobile

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,11 @@ import '../sass/style.scss';
 const burger = document.querySelector('.burger');
 const burgerList = document.querySelector('.burger-list');
 
-burger.addEventListener('click', () => {
-  
+
+const listener = () => {
   burger.classList.toggle('burger-active');
   burgerList.classList.toggle('burger-list-active');
+};
 
-})
-
-
-
+burger.addEventListener('click', listener);
 
